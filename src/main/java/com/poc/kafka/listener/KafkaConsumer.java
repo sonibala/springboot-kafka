6a26d52583a93847e3bd85dc1ecd6e8d02d92401
Sonia Balamurugan <sonia.balamurugan@perficient.com>
2021-04-09 20:19:15 +0530

    KafkaConsumerImpl commit

---
+++ src/main/java/com/poc/kafka/listener/KafkaConsumer.java
@@ -0,0 +1,23 @@
+package com.poc.kafka.listener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.poc.kafka.model.User;
+
+@Service
+public class KafkaConsumer {
+
+	@KafkaListener(topics = "Kafka_Example", groupId = "group_id")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+
+    @KafkaListener(topics = "Kafka_Example_json", groupId = "group_json",
+            containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " + user);
+    }
+    
+}
---
+++ src/test/java/com/poc/kafka/KafkaConsumerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
